2adc1b09f4030e65c69b3f4208c5301dc84bd9cd
ayeshashaikh172005 <ayeshashaikh09133@gmail.com>
2025-09-07 10:40:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1072,6 +1072,6 @@ Output format:
 
 # -------------------- Run App --------------------
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5050, debug=True, use_reloader=False)
+    socketio.run(app, host="0.0.0.0", port=8000, debug=True, use_reloader=False)
 
 
