fdaace9bc40489bc4af9b1c9044718c3d89378a8
ayeshashaikh172005 <ayeshashaikh09133@gmail.com>
2025-09-07 09:57:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1072,6 +1072,6 @@ if __name__ == "__main__":
     import eventlet
     eventlet.monkey_patch()
     from app import app, socketio
-    socketio.run(app, host="0.0.0.0", port=5050, debug=False)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
 
 
